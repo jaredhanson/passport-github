38a52c788596d4c4d3d1c705c25608a9a33983d1
Jared Hanson <jaredhanson@gmail.com>
2011-11-23 19:02:35 -0800

    Initial commit.

---
+++ test/strategy-test.js
@@ -0,0 +1,105 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var GitHubStrategy = require('passport-github/strategy');
+
+
+vows.describe('GitHubStrategy').addBatch({
+  
+  'strategy': {
+    topic: function() {
+      return new GitHubStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+    },
+    
+    'should be named github': function (strategy) {
+      assert.equal(strategy.name, 'github');
+    },
+  },
+  
+  'strategy when loading user profile': {
+    topic: function() {
+      var strategy = new GitHubStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+        var body = '{ "login": "octocat", "id": 1, "name": "monalisa octocat", "email": "octocat@github.com", "html_url": "https://github.com/octocat" }';
+        
+        callback(null, body, undefined);
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should not error' : function(err, req) {
+        assert.isNull(err);
+      },
+      'should load profile' : function(err, profile) {
+        assert.equal(profile.provider, 'github');
+        assert.equal(profile.id, '1');
+        assert.equal(profile.username, 'octocat');
+        assert.equal(profile.displayName, 'monalisa octocat');
+        assert.equal(profile.profileUrl, 'https://github.com/octocat');
+        assert.length(profile.emails, 1);
+        assert.equal(profile.emails[0].value, 'octocat@github.com');
+      },
+    },
+  },
+  
+  'strategy when loading user profile and encountering an error': {
+    topic: function() {
+      var strategy = new GitHubStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+        callback(new Error('something-went-wrong'));
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should error' : function(err, req) {
+        assert.isNotNull(err);
+      },
+      'should not load profile' : function(err, profile) {
+        assert.isUndefined(profile);
+      },
+    },
+  },
+  
+}).export(module);
