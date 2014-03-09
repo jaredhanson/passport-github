b695ee2e2ae51667a9d96dce3630dedcf5c91b2b
Jared Hanson <jaredhanson@gmail.com>
2014-03-09 09:54:34 -0700

    Update support files.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('passport-github', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
