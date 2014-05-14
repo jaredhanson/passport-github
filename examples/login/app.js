c8bdeda1b6e48e2ea7fc493a54227ac9d9fd6db7
Alberto Alcaraz <jaalcarazz@yahoo.com>
2014-05-14 16:27:10 -0500

    Simplified the success redirect

--- examples/login/app.js
+++ examples/login/app.js
@@ -95,11 +95,7 @@ app.get('/auth/github',
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/auth/github/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }));
 
 app.get('/logout', function(req, res){
   req.logout();
