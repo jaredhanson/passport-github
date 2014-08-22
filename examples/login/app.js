1ef6348cca1e233996016c46dfc3d9581783018b
Casey Garland <caseyg1204@gmail.com>
2014-08-22 13:26:54 -0400

    updated login example for Express 4

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,6 +1,9 @@
 var express = require('express')
   , passport = require('passport')
   , util = require('util')
+  , bodyParser = require('body-parser')
+  , cookieParser = require('cookie-parser')
+  , session = require('express-session')
   , GitHubStrategy = require('passport-github').Strategy;
 
 var GITHUB_CLIENT_ID = "--insert-github-client-id-here--"
@@ -55,15 +58,14 @@ app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
+  app.use(cookieParser());
+  app.use(bodyParser());
   app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(session({ secret: 'keyboard cat' }));
   // Initialize Passport!  Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
