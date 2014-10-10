658192b2c8cb760a4767b18594fcdfee1cffbdb9
bace135 <bace135@users.noreply.github.com>
2014-10-10 11:56:28 -0700

    Update app.js

--- examples/login/app.js
+++ examples/login/app.js
@@ -83,7 +83,7 @@ app.get('/login', function(req, res){
 // GET /auth/github
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in GitHub authentication will involve redirecting
-//   the user to github.com.  After authorization, GitHubwill redirect the user
+//   the user to github.com.  After authorization, GitHub will redirect the user
 //   back to this application at /auth/github/callback
 app.get('/auth/github',
   passport.authenticate('github'),
@@ -95,7 +95,7 @@ app.get('/auth/github',
 // GET /auth/github/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
+//   login page.  Otherwise, the primary route function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
