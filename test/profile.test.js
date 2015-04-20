1744e32bb213015b2a833ee22eca8baf0b036b9c
Alejandro Tardin <alejandro@tardin.com>
2015-04-20 21:02:08 +0200

    Added picture to the profile from 'avatar_url'

--- test/profile.test.js
+++ test/profile.test.js
@@ -6,10 +6,10 @@ var fs = require('fs')
 
 
 describe('profile.parse', function() {
-    
+
   describe('example profile', function() {
     var profile;
-    
+
     before(function(done) {
       fs.readFile('test/data/example.json', 'utf8', function(err, data) {
         if (err) { return done(err); }
@@ -17,7 +17,7 @@ describe('profile.parse', function() {
         done();
       });
     });
-    
+
     it('should parse profile', function() {
       expect(profile.id).to.equal('1');
       expect(profile.username).to.equal('octocat');
@@ -25,12 +25,14 @@ describe('profile.parse', function() {
       expect(profile.profileUrl).to.equal('https://github.com/octocat');
       expect(profile.emails).to.have.length(1);
       expect(profile.emails[0].value).to.equal('octocat@github.com');
+      expect(profile.photos).to.have.length(1);
+      expect(profile.photos[0].value).to.equal('https://github.com/images/error/octocat_happy.gif');
     });
   });
-  
+
   describe('example profile with null email', function() {
     var profile;
-    
+
     before(function(done) {
       fs.readFile('test/data/example-null-email.json', 'utf8', function(err, data) {
         if (err) { return done(err); }
@@ -38,7 +40,7 @@ describe('profile.parse', function() {
         done();
       });
     });
-    
+
     it('should parse profile', function() {
       expect(profile.id).to.equal('1');
       expect(profile.username).to.equal('octocat');
@@ -47,5 +49,25 @@ describe('profile.parse', function() {
       expect(profile.emails).to.be.undefined;
     });
   });
-  
+
+  describe('example profile with null avatar', function() {
+    var profile;
+
+    before(function(done) {
+      fs.readFile('test/data/example-null-avatar.json', 'utf8', function(err, data) {
+        if (err) { return done(err); }
+        profile = parse(data);
+        done();
+      });
+    });
+
+    it('should parse profile', function() {
+      expect(profile.id).to.equal('1');
+      expect(profile.username).to.equal('octocat');
+      expect(profile.displayName).to.equal('monalisa octocat');
+      expect(profile.profileUrl).to.equal('https://github.com/octocat');
+      expect(profile.photos).to.be.undefined;
+    });
+  });
+
 });
