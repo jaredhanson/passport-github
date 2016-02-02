451db704e3b7e6b72266da3e1fa8f36496cd8881
Jared Hanson <jaredhanson@gmail.com>
2016-02-02 14:16:45 -0800

    Improve email handling.

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -51,6 +51,61 @@ describe('Strategy#userProfile', function() {
     });
   }); // fetched from default endpoint
   
+  describe('fetched from default endpoint and then fetching emails, where user has a publicly visible email address', function() {
+    var strategy =  new GitHubStrategy({
+      clientID: 'ABC123',
+      clientSecret: 'secret',
+      scope: [ 'user:email' ]
+    }, function() {});
+  
+    strategy._oauth2._request = function(method, url, headers, body, accessToken, callback) {
+      var body;
+      switch (url) {
+      case 'https://api.github.com/user':
+        body = '{ "login": "octocat", "id": 1, "name": "monalisa octocat", "email": "octocat@github.com", "html_url": "https://github.com/octocat" }';
+        break;
+      case 'https://api.github.com/user/emails':
+        body = '[{"email":"octocat@github.com","primary":true,"verified":true}]';
+        break;
+      default:
+        return callback(new Error('wrong url argument'));
+      }
+      callback(null, body, undefined);
+    };
+    
+    
+    var profile;
+    
+    before(function(done) {
+      strategy.userProfile('token', function(err, p) {
+        if (err) { return done(err); }
+        profile = p;
+        done();
+      });
+    });
+    
+    it('should parse profile', function() {
+      expect(profile.provider).to.equal('github');
+      
+      expect(profile.id).to.equal('1');
+      expect(profile.username).to.equal('octocat');
+      expect(profile.displayName).to.equal('monalisa octocat');
+      expect(profile.profileUrl).to.equal('https://github.com/octocat');
+      expect(profile.emails).to.have.length(1);
+      expect(profile.emails[0].value).to.equal('octocat@github.com');
+      expect(profile.emails[0].primary).to.equal(true);
+      expect(profile.emails[0].verified).to.equal(true);
+    });
+    
+    it('should set raw property', function() {
+      expect(profile._raw).to.be.a('string');
+    });
+    
+    it('should set json property', function() {
+      expect(profile._json).to.be.an('object');
+    });
+  }); // fetched from default endpoint and then fetching emails, where user has a publicly visible email address
+  
   describe('error caused by invalid token', function() {
     var strategy =  new GitHubStrategy({
         clientID: 'ABC123',
