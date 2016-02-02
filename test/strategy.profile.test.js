2a3fbaf7367680fe4704ff00c99b7afd6e615a92
Jared Hanson <jaredhanson@gmail.com>
2016-02-02 14:49:19 -0800

    More test cases.

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -133,6 +133,32 @@ describe('Strategy#userProfile', function() {
     });
   }); // error caused by invalid token
   
+  describe('error caused by malformed response', function() {
+    var strategy =  new GitHubStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+  
+    strategy._oauth2.get = function(url, accessToken, callback) {
+      var body = 'Hello, world.';
+      callback(null, body, undefined);
+    };
+      
+    var err, profile;
+    before(function(done) {
+      strategy.userProfile('token', function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('Failed to parse user profile');
+    });
+  }); // error caused by malformed response
+  
   describe('internal error', function() {
     var strategy =  new GitHubStrategy({
       clientID: 'ABC123',
