67bc830be284d4bf756969aca4af6bc1adda3d44
Jared Hanson <jaredhanson@gmail.com>
2016-02-02 16:22:29 -0800

    Additional test case.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -72,6 +72,70 @@ describe('Strategy', function() {
     });
   });
   
+  describe('handling a response with an authorization code', function() {
+    var OAuth2Strategy = require('passport-oauth2').Strategy;
+    var OAuth2;
+    if (existsSync('node_modules/oauth')) { // npm 3.x
+      OAuth2 = require('oauth').OAuth2;
+    } else {
+      OAuth2 = require('passport-oauth2/node_modules/oauth').OAuth2;
+    }
+    
+    var MockOAuth2Strategy = function(options, verify) {
+      OAuth2Strategy.call(this, options, verify);
+      
+      this._oauth2 = new OAuth2(options.clientID,  options.clientSecret,
+        '', options.authorizationURL, options.tokenURL, options.customHeaders);
+      this._oauth2.getOAuthAccessToken = function(code, options, callback) {
+        if (code != 'SplxlOBeZQQYbYS6WxSbIA+ALT1') { return callback(new Error('wrong code argument')); }
+        
+        return callback(null, 's3cr1t-t0k3n', undefined, {});
+      };
+      this._oauth2.get = function(url, accessToken, callback) {
+        if (url != 'https://api.github.com/user') { return callback(new Error('wrong url argument')); }
+        if (accessToken != 's3cr1t-t0k3n') { return callback(new Error('wrong token argument')); }
+    
+        var body = '{ "login": "octocat", "id": 1, "name": "monalisa octocat", "email": "octocat@github.com", "html_url": "https://github.com/octocat" }';
+        callback(null, body, undefined);
+      };
+    }
+    util.inherits(MockOAuth2Strategy, OAuth2Strategy);
+    
+    var GitHubStrategy = $require('../lib/strategy', {
+      'passport-oauth2': MockOAuth2Strategy
+    })
+    
+    var strategy = new GitHubStrategy({
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function verify(accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
+        return done(null, profile);
+      })
+    });
+    
+    
+    var user;
+
+    before(function(done) {
+      chai.passport.use(strategy)
+        .success(function(u) {
+          user = u;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA+ALT1';
+        })
+        .authenticate({ display: 'mobile' });
+    });
+
+    it('should authenticate user', function() {
+      expect(user.id).to.equal('1');
+      expect(user.username).to.equal('octocat');
+    });
+  });
+  
   describe('error caused by invalid code sent to token endpoint, with response erroneously indicating success', function() {
     var OAuth2Strategy = require('passport-oauth2').Strategy;
     var OAuth2;
